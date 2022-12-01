6e92bf392345556bfa776e23a64dd3377e0e5d88
mani <manikandan.s@kaaviansys.com>
2022-12-01 14:56:07 +0530

    return status and token

--- try.js
+++
@@ -1,9 +0,0 @@
-const  login  = require('./authentication');
-
-const username = 'mani'
-const password = 'mani123'
-const result = login.generateToken(username,password);
-
-const trial = result 
-// const trial = 'dcfvgtybhujikgbsdcfvgfvgbhjbghnjmk';
-const final = login.checkingToken(trial);
