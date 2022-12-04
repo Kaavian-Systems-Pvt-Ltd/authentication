32c5ee2e2419553751e6bd5c320920a5694b4106
mani <manikandan.s@kaaviansys.com>
2022-12-04 21:03:19 +0530

    add next() in checkToken

--- authentication.js
+++ authentication.js
@@ -1,51 +1,52 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const generateToken = (userName) => {
-    // const name = userName;
-    // const pass = password;
-    if(userName){
-    const token =  jwt.sign({userName}, `${process.env.ACCESS_TOKEN_SECRET}`);
-    console.log( token);
-    return ({status: 'Success', data: token});
-    }else {
-        return({status: 'Failure', data: 'Invalid password'});
-    }
-}
+/**
+ * 
+ * @param {String} userData generates token for the user specified field 
+ * @returns status and generated token
+ * this function is used for generate token 
+ */
+
+const generateToken = (userData) => {
+  if (userData) {
+    const token = jwt.sign({ userData }, `${process.env.ACCESS_TOKEN_SECRET}`);
+    return { status: "Success", data: token };
+  } else {
+    return { status: "Failure", data: "Invalid password" };
+  }
+};
+
+/**
+ * 
+ * @param { {ignorePath: [ignorePaths], token: token, req: req, res: res, next: next} } propertys validate the api's 
+ * @returns status
+ */
 
 const checkingToken = (propertys) => {
-    const ignorePaths = propertys.ignorePath[0];
-    const receivedToken = propertys.token;
-    const req = propertys.req ;
-    const res = propertys.res;
-    const url = req.url;
-    console.log(url, 69);
-    console.log(ignorePaths, 00);
-    if(url === ignorePaths && receivedToken === undefined){
-        return ({status: 'success', data: 'loginApiRequest'});
+  const ignorePaths = propertys.ignorePath[0];
+  const receivedToken = propertys.token;
+  const req = propertys.req;
+  const res = propertys.res;
+  const next = propertys.next;
+  const url = req.url;
+  try {
+    if (url === ignorePaths && receivedToken === undefined) {
+      next();
     }
-    try{
-    if(receivedToken){
-    const token = jwt.verify(receivedToken,`${process.env.ACCESS_TOKEN_SECRET}`);
-    console.log(token);
-    return ({status: 'success', data:token});
+    if (receivedToken === '') {
+      return { status: 'invalidToken'};
     }
-}catch{
-    return ({status:"invalidToken"});
-}
-    return ({status:'can not find'});
-}
+    if (receivedToken) {
+      const token = jwt.verify(receivedToken,`${process.env.ACCESS_TOKEN_SECRET}`);
+      next();
+      return { status: "success", data: token };
+    }
+  } catch {
+    return { status: "invalidToken" };
+  }
+};
 
 module.exports = {
-    generateToken,
-    checkingToken,
-}
-
-
-    //jwt.verify(token,`${process.env.ACCESS_TOKEN_SECRET}`,(err, verifiedJwt) => {
-    //     if(err){
-    //         return(err.message);
-    //     }else {
-    //         const data = verifiedJwt
-    //         return ({status: 'success', data});
-    //     }
-    //    });
+  generateToken,
+  checkingToken,
+};
